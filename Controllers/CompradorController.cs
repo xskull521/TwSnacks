574a5033214543259f3d8f5f1a2434a53e052261
Higor <higoranjos@hotmail.com>
2021-09-20 15:19:59 -0300

    first commit

---
+++ Controllers/CompradorController.cs
@@ -0,0 +1,57 @@
+﻿using CadastroProdutoEntidade;
+using CadastroProdutoDAO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProjectOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompradorController : ControllerBase
+    {
+        // GET: api/<EnderecoController>
+        [HttpGet]
+        public IEnumerable<CompradorEntidade> Get()
+        {
+            var repository = new CompradorDAO();
+            return repository.SelectAllComprador();
+        }
+
+        // GET api/<EnderecoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EnderecoController>
+        [HttpPost]
+        public void Post(int IdComprador, string NomeComprador)
+        {
+
+            var repository = new CompradorDAO();
+            repository.InsertData(IdComprador, NomeComprador);
+        }
+
+        // PUT api/<EnderecoController>/5
+        [HttpPut]
+        public void Put(CompradorEntidade compradorEntidade)
+        {
+            var repository = new CompradorDAO();
+            repository.UpdateData(compradorEntidade);
+        }
+
+        // DELETE api/<EnderecoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var repository = new CompradorDAO();
+            repository.DeleteData(id);
+        }
+    }
+}
---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,57 @@
+﻿using CadastroProdutoDAO;
+using CadastroProdutoEntidade;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        // GET: api/<EnderecoController>
+        [HttpGet]
+        public IEnumerable<CadProdutoEntidade> Get()
+        {
+            var repository = new CadastroDAO();
+            return repository.SelectAllProdutos();
+        }
+
+        // GET api/<EnderecoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EnderecoController>
+        [HttpPost]
+        public void Post(string NomeDoProduto, int Valor)
+        {
+
+            var repository = new CadastroDAO();
+            repository.InsertData(NomeDoProduto, Valor);
+        }
+
+        // PUT api/<EnderecoController>/5
+        [HttpPut]
+        public void Put(CadProdutoEntidade CadProdutoEntidade)
+        {
+            var repository = new CadastroDAO();
+            repository.UpdateData(CadProdutoEntidade);
+        }
+
+        // DELETE api/<EnderecoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var repository = new CadastroDAO();
+            repository.DeleteData(id);
+        }
+    }
+}
+
